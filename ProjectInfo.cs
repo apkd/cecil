703ba4625cd3a79bab88b88c476e58f2232f75b2
tomekpaszek <tomek@unity3d.com>
2017-01-05 15:48:08 +0100

    Bumped version number for Mono.Cecil
    Some breaking changes from upstream were added but the version number
    was not bumped. This caused issues with some tools (like nunit runner)

--- ProjectInfo.cs
+++ ProjectInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible (false)]
 #endif
 
-[assembly: AssemblyVersion ("0.9.6.0")]
-[assembly: AssemblyFileVersion ("0.9.6.0")]
+[assembly: AssemblyVersion ("0.9.66.0")]
+[assembly: AssemblyFileVersion ("0.9.66.0")]
